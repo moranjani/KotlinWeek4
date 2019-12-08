a3caf76503336baaa51fa135d909774b213d1a36
moranjani <morancha@gmail.com>
2019-12-09 01:31:38 +0200

    improvements major one - normalize at creation in a companion object, where the default constructor is private, so we must use the companion object function!

--- Rationals/Task/src/rationals/Rational.kt
+++ Rationals/Task/src/rationals/Rational.kt
@@ -2,89 +2,93 @@ package rationals
 
 import java.lang.IllegalArgumentException
 import java.math.BigInteger
+import java.math.BigInteger.ONE
+import java.math.BigInteger.ZERO
 
-data class Rational(val num: BigInteger, val denom: BigInteger) : Comparable<Rational> {
-    init {
-       require(denom != 0.toBigInteger()) { "Denom cannot be zero" }
+data class Rational
+private constructor(val num: BigInteger, val denom: BigInteger) : Comparable<Rational> {
+    companion object {
+        fun create(num: BigInteger, denom: BigInteger): Rational {
+            return normalize(num, denom)
+        }
+
+        private fun normalize(num: BigInteger, denom: BigInteger): Rational {
+            require(denom != 0.toBigInteger()) { "Denom cannot be zero" }
+            val gcd = num.gcd(denom).abs()
+            val denomSign = denom.signum().toBigInteger()
+            val newNum = num / gcd * denomSign
+            val newDenom = denom / gcd * denomSign
+            return Rational(newNum, newDenom)
+        }
     }
 
-    constructor(x: Int, y: Int): this(x.toBigInteger(), y.toBigInteger())
-
-    constructor(x: Long, y: Long): this(x.toBigInteger(), y.toBigInteger()) {
-        println("Now I'm using the third constructor")
-    }
 
     override fun compareTo(other: Rational): Int {
         return (this.num * other.denom).compareTo(other.num * this.denom)
     }
 
-    override fun equals(other: Any?): Boolean {
-        return (other is Rational && this.num * other.denom == other.num * this.denom)
-    }
+//    override fun equals(other: Any?): Boolean {
+//        return (other is Rational && this.num * other.denom == other.num * this.denom)
+//    }
 
     override fun toString(): String {
-        if (num == 0.toBigInteger()) return "0"
-
-        val gcd = num.gcd(denom).abs()
-        val newNum = num / gcd
-        val newDenom = denom / gcd
-        var res :String = newNum.abs().toString() + "/" + newDenom.abs().toString()
-        when {
-            newDenom == 1.toBigInteger() -> res = newNum.toString()
-            (num * denom) < 0.toBigInteger() -> res = "-$res"
-        }
+        if (num == ZERO) return "0"
+        var res: String = num.toString() + "/" + denom.toString()
+        if (denom == ONE) res = num.toString()
         return res
     }
 
 
-    operator fun plus(other: Rational) : Rational {
+    operator fun plus(other: Rational): Rational {
         var retDenom = denom * other.denom
         val retNum: BigInteger = retDenom / denom * num + retDenom / other.denom * other.num
-        return Rational(retNum, retDenom)
+        return create(retNum, retDenom)
     }
 
-    operator fun minus(other: Rational) : Rational {
+    operator fun minus(other: Rational): Rational {
         var retDenom = denom * other.denom
         val retNum: BigInteger = retDenom / this.denom * this.num - retDenom / other.denom * other.num
-        return Rational(retNum, retDenom)
+        return create(retNum, retDenom)
     }
 
-    operator fun times(other: Rational) : Rational {
-        return Rational(this.num * other.num, this.denom * other.denom)
+    operator fun times(other: Rational): Rational {
+        return create(this.num * other.num, this.denom * other.denom)
     }
 
-    operator fun div(other: Rational) : Rational {
-        return Rational(this.num * other.denom, this.denom * other.num)
+    operator fun div(other: Rational): Rational {
+        return create(this.num * other.denom, this.denom * other.num)
     }
 
-    operator fun unaryMinus() : Rational {
-        var newNum = num.abs()
-        if (this.num * this.denom > 0.toBigInteger()) {
-            newNum = -newNum
-        }
-        return Rational(newNum, denom.abs())
+    operator fun unaryMinus(): Rational {
+        return create(-num, denom)
     }
-
 }
 
+
 infix fun Int.divBy(other: Int): Rational {
-    return Rational(this, other)
+    return Rational.create(this.toBigInteger(), other.toBigInteger())
 }
 
 infix fun Long.divBy(other: Long): Rational {
-    return Rational(this, other)
+    return Rational.create(this.toBigInteger(), other.toBigInteger())
 }
 
 infix fun BigInteger.divBy(other: BigInteger): Rational {
-    return Rational(this, other)
+    return Rational.create(this, other)
 }
 
-fun String.toRational() : Rational {
-    if ("/" !in this) return Rational(this.toBigInteger(), 1.toBigInteger())
-    val split = this.split("/")
-    return Rational(split[0].toBigInteger(), split[1].toBigInteger())
-}
+fun String.toRational(): Rational {
+    fun String.toBigIntegerOrThrow() :BigInteger {
+        val errMessage = "we are expecting a string of format int/int. We got $this"
+        return this.toBigIntegerOrNull() ?: throw IllegalArgumentException(errMessage)
+    }
 
+    if ("/" !in this) {
+        return Rational.create(this.toBigIntegerOrThrow(), ONE)
+    }
+    val (numText, denomText) = this.split("/")
+    return Rational.create(numText.toBigIntegerOrThrow(), denomText.toBigIntegerOrThrow())
+}
 
 
 fun main() {
